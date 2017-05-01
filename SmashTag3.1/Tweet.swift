12b33c7795383630827b57afec56f245ebaf7574
Lawrence Flancbaum <lawrencef@macbookpro15.local>
2017-05-02 07:16:44 +1000

    getting could not build objective-c module twitter error. attempted solution ... pre convert twitter framework to modern swift syntax.

---
+++ SmashTag3.1/Tweet.swift
@@ -0,0 +1,28 @@
+//
+//  Tweet.swift
+//  SmashTag3.1
+//
+//  Created by Lawrence Flancbaum on 2/5/17.
+//  Copyright © 2017 Cloudmass. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Tweet: NSManagedObject {
+    
+    
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ SmashTag3.1/TwitterUser.swift
@@ -0,0 +1,14 @@
+//
+//  TwitterUser.swift
+//  SmashTag3.1
+//
+//  Created by Lawrence Flancbaum on 2/5/17.
+//  Copyright © 2017 Cloudmass. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TwitterUser: NSManagedObject {
+
+}

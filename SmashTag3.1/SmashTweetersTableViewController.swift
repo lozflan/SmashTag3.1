aae1ac144154dba966834087b3a99b3ca6c51cf3
Lawrence Flancbaum <lawrencef@macbookpro15.local>
2017-05-08 14:46:03 +1000

    tweetertvc working showing table of tweeters mentioning search term and the number of tweets each twitteruser has tweeted (mentioning that mention). about to attempt Smashtag mentions assignment

--- SmashTag3.1/SmashTweetersTableViewController.swift
+++ SmashTag3.1/SmashTweetersTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Twitter
 import CoreData
 
-class SmashTweetersTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+class SmashTweetersTableViewController: FetchedResultsTableViewController {
 
  
     //model 
@@ -19,37 +19,63 @@ class SmashTweetersTableViewController: UITableViewController, NSFetchedResultsC
     
     var container: NSPersistentContainer? = AppDelegate.container { didSet { updateUI() }}
     
-    var frc: NSFetchedResultsController<TwitterUser>? {
-        didSet {
-            frc?.delegate = self
-        }
-    }
+    var fetchedResultsController: NSFetchedResultsController<TwitterUser>?
     
 
     private func updateUI() {
-        //ensure tableview is a frc delegate
-        
-        //create new freq and frc with the mention
-        let request: NSFetchRequest<TwitterUser> = TwitterUser.fetchRequest()
-        let sortDescriptor = NSSortDescriptor(key: "handle", ascending: true, selector: #selector(NSString.localizedStandardCompare(_:)))
-        request.sortDescriptors = [sortDescriptor]
-        if let results = try? container?.viewContext.fetch(request) {
+        if let context = container?.viewContext, mention != nil {
+            //create new freq and frc with the mention
+            let request: NSFetchRequest<TwitterUser> = TwitterUser.fetchRequest()
+            let sortDescriptor = NSSortDescriptor(key: "handle", ascending: true, selector: #selector(NSString.localizedStandardCompare(_:)))
+            let predicate = NSPredicate(format: "any tweets.text contains[c] %@", mention!)
+            request.sortDescriptors = [sortDescriptor]
+            request.predicate = predicate
             
+            //create a frc instance if the mention (or container) is updated
+            fetchedResultsController = NSFetchedResultsController(
+                fetchRequest: request,
+                managedObjectContext: context,
+                sectionNameKeyPath: nil,
+                cacheName: nil)
+            //perform the actual fetch
+            try? fetchedResultsController?.performFetch()
+            //set ourselves as the frc delegate
+            self.fetchedResultsController?.delegate = self
+            //get the tableview to call its required methods
+            tableView.reloadData()
         }
-        
-        //Thursday, 4 May 2017 UP TO HERE 
-//        let frc: NSFetchedResultsController<TwitterUser> = NSFetchedResultsController(fetchRequest: <#T##NSFetchRequest<NSFetchRequestResult>#>, managedObjectContext: <#T##NSManagedObjectContext#>, sectionNameKeyPath: <#T##String?#>, cacheName: <#T##String?#>)
-        //execute the freq
-        
-        tableView.reloadData()
     }
     
 
-
-    override func viewDidLoad() {
-        
-        
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TwitterUser Cell", for: indexPath)
+        if let twitterUser = fetchedResultsController?.object(at: indexPath) {
+            cell.textLabel?.text = twitterUser.name
+            let tweetCount = tweetCountWithMentionBy(twitterUser)
+                
+                
+            cell.detailTextLabel?.text = "\(tweetCount) tweet" + "\((tweetCount) > 1 ? "s" : "")"
+        }
+        return cell
     }
+    
+    private func tweetCountWithMentionBy(_ twitterUser: TwitterUser) -> Int {
+        //you have a twitterUser so use that MO's context
+        let request: NSFetchRequest<Tweet> = Tweet.fetchRequest()
+        let predicate = NSPredicate(format: "text contains[c] %@ AND tweeter = %@", mention!, twitterUser)
+        request.predicate = predicate
+
+        let count = (try? twitterUser.managedObjectContext!.count(for: request)) ?? 0
+        return count
+        
+//            
+//            if let tweets = try? context.fetch(request) {
+//                return tweets.count
+//            } else {
+//                return 0
+//            }
+    }
+ 
 
 
 
